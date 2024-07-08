84500dd589eca3f420a08c23dce52ec60eca5cde
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-07-08 03:01:10 +0000

    Adding is debug to new components

--- app/main.py
+++ app/main.py
@@ -4,6 +4,6 @@ from obregon_1516_1617_xgboost import do_run_xgb
 
 
 if __name__ == "__main__":
-    # do_run_svr(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_svr.csv")
+    do_run_svr(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_svr.csv")
     # do_run_rf(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_rf.csv")
-    do_run_xgb(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_xgb.csv")
+    # do_run_xgb(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_xgb.csv")
--- app/obregon_1516_1617_svr.py
+++ app/obregon_1516_1617_svr.py
@@ -36,29 +36,44 @@ def do_run_svr(do_basic: bool, do_heatmap: bool, do_pca: bool, file_name: str ,i
         print("Start - work")
     for kernel in kernels:
         if do_basic:
+            if is_debug:
+                print("Do with original data")
             for file_csv in files_csv_original:
+                if is_debug:
+                    print(f"File = {file_csv}")
                 data = run_svr(
                     reductionName="Without",
                     file_csv=file_csv, 
                     kernel=kernel, 
-                    data=data
+                    data=data,
+                    is_debug=is_debug,
                 )
         if do_heatmap:
+            if is_debug:
+                print("Do with heatmap data")
             for file_csv in files_csv_heatmap:
+                if is_debug:
+                    print(f"File = {file_csv}")
                 data = run_svr(
                     reductionName="heatmap",
                     file_csv=file_csv, 
                     kernel=kernel, 
-                    data=data
+                    data=data,
+                    is_debug=is_debug,
                 )
         if do_pca:
+            if is_debug:
+                print("Do with PCA data")
             for file_csv in files_csv_original:
+                if is_debug:
+                    print(f"File = {file_csv}")
                 data = run_svr(
                     reductionName="pca", 
                     file_csv=file_csv, 
                     kernel=kernel,
                     data=data, 
                     do_pca=True,
+                    is_debug=is_debug,
                 )
     df = pd.DataFrame(
         data, columns=['ML', 'Param','Reduction Technic' ,'Dataset', 'RMSE', 'R2', 'MAPE', "Number fo Components"]
@@ -67,7 +82,7 @@ def do_run_svr(do_basic: bool, do_heatmap: bool, do_pca: bool, file_name: str ,i
     df.to_csv(file_name, index=False)
 
 
-def run_svr(reductionName: str, file_csv: str, kernel, data, do_pca: bool = False):
+def run_svr(reductionName: str, file_csv: str, kernel, data, do_pca: bool = False, is_debug: bool = False):
     datasetName = f"{
         file_csv
         .replace("data/obregon_1516_1617/", "")
@@ -77,7 +92,7 @@ def run_svr(reductionName: str, file_csv: str, kernel, data, do_pca: bool = Fals
         .replace("_", " ")}"
     ml = SVR_Prediction(kernel=kernel)
     preprocessing = Preprocess(
-        file_name=file_csv, type_file=TypeFileEnum.CSV)
+        file_name=file_csv, type_file=TypeFileEnum.CSV, is_debug=is_debug)
     if do_pca:
         preprocessing.read_file(
             transform=TransformEnum.PCA,
@@ -91,8 +106,10 @@ def run_svr(reductionName: str, file_csv: str, kernel, data, do_pca: bool = Fals
         )
     preprocessing.build_train_and_test()
     x_train, y_train = preprocessing.get_train()
+   
     x_test, y_test = preprocessing.get_test()
-
+    if is_debug:
+        print (f"train size => {x_train.shape} , test size => {x_test.shape} ")
     ml.training(x_train=x_train, y_train=y_train)
     y_predicted = ml.prediction(x_test=x_test)
     metric = ErrorMetric(y_predicted=y_predicted,
