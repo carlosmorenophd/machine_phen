e08d5c0f85e2a4f256cdf60f3e43ffd20c930f79
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-08-26 20:04:51 -0600

    Adding new function to search in all

--- app/main.py
+++ app/main.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     launch_all()
     print("Finish")
     # do_run_svr(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_svr.csv")
-    # do_run_rf(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_rf.csv")
+    do_run_rf(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_rf.csv")
     # do_run_xgb(do_basic=True, do_heatmap=True, do_pca=True, file_name="result_test_xgb.csv")
