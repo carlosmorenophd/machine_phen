09a3b856a258a965ae03319602db1de0d751c2f3
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2024-11-04 19:13:18 -0600

    Adding to new way to use the repo and celery

--- tasks/src/preprocesses/preprocess.py
+++ tasks/src/preprocesses/preprocess.py
@@ -1,14 +1,18 @@
-import pandas as pd
-from numpy import ndarray
-from sklearn.impute import SimpleImputer
-from sklearn.preprocessing import StandardScaler
-from preprocesses.enums import TypeFileEnum, TransformEnum, StandardScaleEnum
-from sklearn.model_selection import train_test_split
+"""Preprocessing data to machines"""
 from typing import Tuple
+import pandas as pd
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+
+from preprocesses.enums import TypeFileEnum, TransformEnum, StandardScaleEnum
 from preprocesses.pca_preprocess import PCA_Preprocess
 
 
 class Preprocess:
+    """Main class for preprocessing"""
+
     def __init__(
         self, file_name: str, type_file: TypeFileEnum, is_debug: bool = False
     ) -> None:
@@ -20,6 +24,12 @@ class Preprocess:
         self.y_train = None
         self.y_test = None
         self.x_transform = None
+        self.transform = None
+        self.dataset = None
+        self.x = None
+        self.y = None
+        self.imputer = None
+        self.pca = None
 
     def read_file(
         self,
@@ -27,6 +37,7 @@ class Preprocess:
         standard_scale: StandardScaleEnum = StandardScaleEnum.PASS,
         is_search_best_pca_component: bool = False,
     ) -> None:
+        """Read file"""
         self.transform = transform
         if self.type_file == TypeFileEnum.CSV:
             self.dataset = pd.read_csv(self.file_name)
@@ -69,6 +80,7 @@ class Preprocess:
         x,
         y,
     ) -> None:
+        """Function to set data"""
         self.x = x
         self.y = y
         self.x_transform = x
@@ -76,6 +88,7 @@ class Preprocess:
     def build_train_and_test(
         self, test_size: float = 0.2, random_state: int = 42
     ) -> None:
+        """Create train and test"""
         self.test_size = test_size
         self.random_state = random_state
         self.x_train, self.x_test, self.y_train, self.y_test = train_test_split(
@@ -83,38 +96,43 @@ class Preprocess:
         )
 
     def get_parameter_train_test(self) -> Tuple:
+        """Get parameter for train and test"""
         return self.test_size, self.random_state
 
     def get_pca(self, threshold: float = 0.95):
+        """Get from PCA"""
         if self.transform == TransformEnum.PCA:
             self.pca.evaluate_pca(n_components=None, threshold=threshold)
             return self.pca
 
     def get_train(self):
+        """Get train"""
         if self.x_train is None:
-            raise Exception(
+            raise ModuleNotFoundError(
                 "Sorry, you need to set the method build_train_and_test before call this method"
             )
-        else:
-            return self.x_train, self.y_train
+        return self.x_train, self.y_train
 
     def get_test(self):
+        """Get test"""
         if self.x_train is None:
-            raise Exception(
+            raise ModuleNotFoundError(
                 "Sorry, you need to set the method set_train_and_test before call this method"
             )
-        else:
-            return self.x_test, self.y_test
+        return self.x_test, self.y_test
 
-    def get_name_of_column_by_index(self, list_index):
+    def get_name_of_column_by_index(self, list_index: list) -> list:
+        """Get name of columns by index"""
         column_header = self.dataset.columns.values
         result = []
         for index in list_index:
             result.append(column_header[index])
         return result
 
-    def get_name_of_column(self):
+    def get_name_of_column(self) -> list:
+        """Get list of columns """
         return self.dataset.columns.values
 
-    def get_name_features(self):
+    def get_name_features(self) -> list:
+        """Get name of features"""
         return self.dataset.columns.values[:-1]
