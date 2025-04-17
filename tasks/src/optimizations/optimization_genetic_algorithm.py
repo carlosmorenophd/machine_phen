99d3eb2c8e7c9e3ca5a556c74a360da9a86ee732
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2025-04-17 03:14:57 +0000

    Clear las individual

--- tasks/src/optimizations/optimization_genetic_algorithm.py
+++ tasks/src/optimizations/optimization_genetic_algorithm.py
@@ -267,6 +267,7 @@ class GeneticAlgorithm():
     ) -> None:
         """Create the initial population
         """
+        self._population.clear()
         for _ in range(population_number):
             self._population.append(
                 self._create_initial_individual(
@@ -519,11 +520,12 @@ class GeneticAlgorithm():
         """Main function to run the genetic algorithm
         """
         for machine in self._genetic_parameters.machines_key:
+            self._log.initial_log()
             self._create_initial_population(
                 population_number=self._genetic_parameters.number_population,
                 machines=[machine]
             )
-            self._log.initial_log()
+            self._log.log_message(quick_message="Finish first population")
             self._current_generation_number = 1
             while self._stop_genetic_algorithm():
                 self._log.progress_log(
