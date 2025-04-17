4340a898da1e14ff5b252c1b8b1ba741ec3aa07a
Juan Carlos Moreno Sanchez <carlos.moreno.phd@gmail.com>
2025-04-17 02:56:46 +0000

    Adding new way to search machines

--- tasks/src/optimizations/optimization_genetic_algorithm.py
+++ tasks/src/optimizations/optimization_genetic_algorithm.py
@@ -313,7 +313,7 @@ class GeneticAlgorithm():
                 selected and one machine by random
         """
         machines_chose = self._genetic_parameters.machines_key
-        if machines is not None:
+        if len(machines) != 0:
             machines_chose = machines
         genetic_individual = GeneticIndividual(
             machine=machine_build_regression_optimization_decimal(
